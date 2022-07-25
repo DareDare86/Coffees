2ab378bb4818de3a06a234632687a8f7517646df
DareDare86 <arsedk@gmail.com>
2022-07-25 10:02:46 +0200

    ok

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const date = (document.getElementById("date").innerHTML =
 const navBtn = document.getElementById("nav-btn");
 const navbar = document.getElementById("navbar");
 const navClose = document.getElementById("nav-close");
+const navAHrefClose = document.getElementById("href-close");
 
 //show nav
 navBtn.addEventListener("click", () => {
@@ -14,5 +15,9 @@ navBtn.addEventListener("click", () => {
 
 //close nav
 navClose.addEventListener("click", () => {
+  navbar.classList.remove("showNav");
+});
+
+navAHrefClose.addEventListener("click", function () {
   navbar.classList.remove("showNav");
 });
